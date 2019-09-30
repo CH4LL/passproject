731cd8a48d63218be0b0348f1661e645d619318c
Jerry <52690534+CH4LL@users.noreply.github.com>
2019-09-30 11:43:45 +0200

    Add files via upload

---
+++ public/bin/notepad/notepad/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    system("C:\\Windows\\system32\\notepad.exe");
+
+    return 0;
+}
---
+++ public/bin/shutdown/shutdown/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    system("C:\\Windows\\System32\\shutdown /i");
+    return 0;
+}
